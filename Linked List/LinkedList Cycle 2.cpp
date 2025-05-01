903db00b356603329ad2aec67410d8e588f3507b
hasmita-atmakuri <atmakurihasmita@gmail.com>
2025-05-01 23:01:09 +0530

    LL Cycle 2

---
+++ Linked List/LinkedList Cycle 2.cpp
@@ -0,0 +1,22 @@
+class Solution {
+  public:
+    ListNode *detectCycle(ListNode *head) {
+      if(head==NULL || head->next==NULL)
+          return NULL;
+      ListNode *fast=head;
+      ListNode *slow=head;
+      while(fast!=NULL && fast->next!=NULL){
+        fast=fast->next->next;
+        slow=slow->next;
+        if(slow==fast){
+          ListNode *start=head;
+          while(start!=slow){
+            start=start->next;
+            slow=slow->next;
+          }
+          return start;
+        }
+      }
+      return NULL;
+    }
+};
